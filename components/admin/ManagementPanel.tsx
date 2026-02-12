357d81806d1697e2d464e5fc5a9ff06a352483fa
website1975 <trieuhaminh@gmail.com>
2026-02-13 06:36:51 +0700

    Create ManagementPanel.tsx

---
+++ components/admin/ManagementPanel.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import TeacherManagement from '../TeacherManagement';
+
+const ManagementPanel: React.FC = () => {
+  return (
+    <div className="h-full animate-in fade-in">
+       <TeacherManagement />
+    </div>
+  );
+};
+
+export default ManagementPanel;
